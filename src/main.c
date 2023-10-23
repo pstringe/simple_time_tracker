953bd9491fcb40199fc27673cc2b9f047f950db6
Poitier Stringer <stringerpoitier@gmail.com>
2023-10-23 02:24:10 +0100

    add datastructures for duration and generation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
 #include "foundation.h"
+#include "time.h"
+
+typedef struct s_duration {
+    time_t      *start;
+    time_t      *end;
+    double      duration;
+}              t_generation;
+
+typedef struct s_generation {
+    char                *name;
+    struct s_duration   *duration;
+
+}              t_generation;
 
 int main (void) {
     return (0);
